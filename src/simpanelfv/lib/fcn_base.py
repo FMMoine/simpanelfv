2ebe7d54c972daa954064a1e00d3bc0c9d8f3f05
FMMoine <moine.francisco04@outlook.com>
2025-11-07 00:16:59 -0300

    Primera instanca de fnc_base e implementacion de ejemplo

--- src/simpanelfv/lib/fcn_base.py
+++ src/simpanelfv/lib/fcn_base.py
@@ -1,2 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def test():
     print("The package has been succesfully loaded!")
+
+# Clase base de generador fotovoltaico fundamental
+class GenPanFV:
+    def __init__(self, Ppico, N, kp, eta, Pinv, mu=2, Gstd = 1000, Tr=25):
+        self.Ppico = Ppico
+        self.kp = kp
+        self.N = N
+        self.eta = eta
+        self.Pinv = Pinv
+        self.mu = mu
+        self.Gstd = Gstd
+        self.Tr = Tr
+        # self.pot_med = 0
+
+    def pot_modelo_GFV(self, G, T):
+        """
+        Devuelve la potencia generada por un GFV con los datos indicados, cuando la irradiancia es G y la temperatura ambiente es T.
+        """
+        Tc = T + 0.031 * G
+        P = self.N * (G / self.Gstd) * self.Ppico * (1 + self.kp * (Tc - self.Tr)) * self.eta * 1e-3
+
+        return P
+
+    def pot_generada_rango(self, lista_G, lista_T):
+        """
+        Simil anterior, pero recibe en lista_G una lista (o vector) con
+        cualquier cantidad de valores de irradiancia, y en lista_T una
+        con igual cantidad de registros de temperatura ambiente.
+        Devuelve otra lista (o contenedor) con las potencias generadas para
+        cada par de valores de irradiancia y temperatura.
+
+        """
+
+        Tc = lista_T + 0.031 * lista_G
+        P = self.N * (lista_G / self.Gstd) * self.Ppico * (1 + self.kp * (Tc - self.Tr)) * self.eta * 1e-3
+
+        return P
+
+
+# Functiones que manipulan la informacion obtenida a traves de los datos ingresados (ir lista de potencia generada)
+def pot_media():
+    """
+    Recibe los mismos argumentos que la función anterior, y devuelve
+    la potencia que resulta de promediar todas las calculadas con
+    cada par de valores de irradiancia y temperatura ambiente.
+    """
+
+def energia():
+    """
+    # Recibe los mismos argumentos que la función anterior, y devuelve
+    # la energía generada por el GFV (en kWh), asumiendo que el intervalo
+    # de tiempo transcurrido entre 2 mediciones de irradiancia (o de temp.)
+    # es de 10 minutos.
+    """
--- src/simpanelfv/main.py
+++ src/simpanelfv/main.py
@@ -1,3 +1,27 @@
-from lib import fcn_base
+import os
 
-fcn_base.test()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from lib import fcn_base as base
+
+base.test()
+
+gen_facultad = base.GenPanFV(240, 12, -4.4e-3, 0.97, 2.5, 2)
+
+pot_punt = gen_facultad.pot_modelo_GFV(750, 25)
+print(pot_punt)
+
+datos = pd.read_excel("extra/Datos_climatologicos_Santa_Fe_2019.xlsx", index_col=0)
+print(datos)
+lista_G = datos['Irradiancia (W/m²)']
+lista_T = datos['Temperatura (°C)']
+# Irradiancia (W/m²)  Temperatura (°C)
+
+pot_rang = gen_facultad.pot_generada_rango(lista_G, lista_T)
+
+print(pot_rang)
+
+plt.plot(pot_rang)
+plt.show()
