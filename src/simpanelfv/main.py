8027a3d712888c8960f2260c9a257592c51fc662
FMMoine <moine.francisco04@outlook.com>
2025-11-28 11:52:33 -0300

    charts test

--- src/simpanelfv/main.py
+++ src/simpanelfv/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import plotly.express as px
 
 from lib.fcn_base import lib_test, GenPanFV
 
@@ -37,4 +38,11 @@ st.latex(r"""
     \frac{a}{b}
 """)
 
-# ejemplo_tabla = 
+ejemplo_tabla = pd.DataFrame(data={
+    "Valores": [1, 2, 3, 4, 5, 6],
+    "Categorias": ["A", "B", "C", "D", "E", "F"]
+})
+
+st.dataframe(ejemplo_tabla)
+fig = px.pie(ejemplo_tabla, names='Categorias', values='Valores')
+st.plotly_chart(fig)
