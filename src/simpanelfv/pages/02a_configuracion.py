f42b8fa8afbe51c2aaadf4d945ceb4454e5dd1ad
FMMoine <moine.francisco04@outlook.com>
2025-11-28 13:00:56 -0300

    Actualizacion config generador

--- src/simpanelfv/pages/02a_configuracion.py
+++ src/simpanelfv/pages/02a_configuracion.py
@@ -6,8 +6,12 @@ import plotly.express as px
 
 from lib.fcn_base import lib_test, GenPanFV
 
-arch_data = st.file_uploader(label='Carga Excel', accept_multiple_files=False)
-
 st.write("""
 # Configuración del sistema generador
          """)
+
+arch_data = st.file_uploader(label='Carga Excel', accept_multiple_files=False)
+
+if arch_data is not None:
+    tabla = pd.read_excel(arch_data)
+    st.dataframe(tabla)
